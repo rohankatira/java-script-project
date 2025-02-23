6fe70d0345b672d4bb432bcc4b1d45b39eb085d3
rohankatira <rohankatira1@gmail.com>
2025-02-23 20:08:19 +0530

    change

---
+++ project-5/Js String Array Methods/Beginner to Advanced Programs in array method/Using map/Q-1 Square Each Number/js/script.js
@@ -0,0 +1,3 @@
+const number = [1,2,3,4,5];
+const squares = number.map(number => number * number);
+console.log(squares);
---
+++ project-5/Js String Array Methods/Beginner to Advanced Programs in array method/Using map/Q-2 Convert Strings to Uppercase/js/script.js
@@ -0,0 +1,3 @@
+const Words = ["apple" , "banana" , "Cherry"];
+const Upper_case = Words.map(Words => Words.toUpperCase());
+console.log(Upper_case);
